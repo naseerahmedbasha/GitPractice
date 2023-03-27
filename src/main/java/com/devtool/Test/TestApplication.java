8c9b79d33ad70de8a31ed0608ce64fc01f9be851
Naseer <nasnaseer6@gmail.com>
2023-03-27 13:15:47 +0530

    third commit

--- src/main/java/com/devtool/Test/TestApplication.java
+++ src/main/java/com/devtool/Test/TestApplication.java
@@ -1,13 +1,14 @@
 package com.devtool.Test;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+		SpringApplication.run(TestApplication.class,args);
 	}
 
 }
